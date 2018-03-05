b69ee41b871355cbc7b4b2572aa741ab52cf8558
Jared Oyler <jaredwo@gmail.com>
2018-03-05 15:27:10 -0500

    fixed package namespace issues for netcdf4 and h5

--- R/obs_netcdf.R
+++ R/obs_netcdf.R
@@ -1,5 +1,5 @@
 #' Load station metadata as a SpatialPointsDataFrame
-#' 
+#'
 #' @param ds H5File object pointing to netCDF observation file
 #' @param vnames Variables names of station metadata to load,
 #'   Default: c("station_id", "station_name", "longitude", "latitude", "elevation")
@@ -13,53 +13,53 @@ load_stns <- function(ds,
                       vnames = c("station_id", "station_name", "longitude", "latitude", "elevation"),
                       vname_stnid = "station_id",
                       vnames_xy = c("longitude", "latitude")) {
-  
+
   stns <- list()
-  
+
   for (vname in vnames) {
-    
+
     vals <- ds[vname][]
-    
+
     # Create vector of strings if 2D character
     if (is.character(vals) & length(dim(vals)) == 2) {
       vals <- apply(vals, 1, paste0, collapse = "")
     }
-    
+
     stns[[vname]] <- vals
-    
+
   }
-  
+
   stns[["obs_index"]] <- 1:length(stns[[1]])
-  
+
   # Turn station dataframe into spatial dataframe
   stns <- as.data.frame(stns, stringsAsFactors = FALSE)
   row.names(stns) <- stns[, vname_stnid]
   sp::coordinates(stns) <- as.formula(paste0("~", vnames_xy[1], "+", vnames_xy[2]))
   sp::proj4string(stns) = sp::CRS("+proj=longlat +datum=WGS84")
-  
+
   return(stns)
-  
+
 }
 
 #' Load time data as a vector of POSIXct times
-#' 
+#'
 #' @param ds H5File object pointing to netCDF observation file
 #' @param vname_time Variable name representing time, Default: "time"
 #' @return POSIXct vector of times
 #' @export
 load_time <- function(ds, vname_time = "time") {
-  
+
   times <- ds[vname_time][]
   times <- RNetCDF::utcal.nc(h5::h5attr(ds[vname_time], "units"), times, type = "c")
   return(times)
-  
+
 }
 
 #' Quickly load station metadata and observations from a netCDF observation file
-#' 
+#'
 #' @param fpath_ds File path to netCDF observation file
 #' @param vname Variable name for which to load observations
-#' @param start_end Character vector of size 2 with start and end dates, 
+#' @param start_end Character vector of size 2 with start and end dates,
 #'   Default: loads observations for all times in the netCDF file
 #' @param stnids Station ids for which to load observations,
 #'   Default: loads observations for all stations in the netCDF file
@@ -67,272 +67,272 @@ load_time <- function(ds, vname_time = "time") {
 #'   SpatialPointsDataFrame and observations are in a spacewise xts object.
 #' @export
 quick_load_stnobs <- function(fpath_ds, vname, start_end = NULL, stnids = NULL) {
-  
+
   # Open netcdf observation file
   ds <- h5::h5file(fpath_ds, mode='r')
-  
+
   # Load station metadata as SpatialPointsDataFrame
   stns <- load_stns(ds)
 
   # Load dates for the observation time series
   times <- load_time(ds)
-  
+
   # Load observations for dates in chunk
   # Observations are returned as an xts object
   obs <- load_obs(ds, vname, stns, times, start_end, stnids)
-  
+
   # Close netcdf observation file
   h5::h5close(ds)
-  
+
   if (!is.null(stnids)) stns <- stns[stnids,]
-  
+
   return(list(stns=stns, obs=obs))
-  
+
 }
 
 #' Load station observation time series as a "spacewide" xts object
-#' 
+#'
 #' @param ds H5File object pointing to netCDF observation file
 #' @param vname Variable name for which to load observations
 #' @param stns SpatialPointsDataFrame of station metadata from \code{\link{load_stns}}
 #' @param times POSIXct vector of times for the netCDF observation file from
 #'   \code{\link{load_time}}
-#' @param start_end Character vector of size 2 with start and end dates, 
+#' @param start_end Character vector of size 2 with start and end dates,
 #'   Default: loads observations for all times in the netCDF file
 #' @param stnids Station ids for which to load observations,
 #'   Default: loads observations for all stations in the netCDF file
 #' @return A spacewide xts object of observations
 #' @export
 load_obs <- function(ds, vname, stns, times, start_end = NULL, stnids = NULL) {
-  
+
   if (is.null(start_end) & is.null(stnids)) {
-    
+
     stnids <- stns$station_id
     obs <- ds[vname][]
-    
+
   } else if (is.null(start_end) & (!is.null(stnids))) {
-    
+
     i <- stns@data[stnids, "obs_index"]
-    
-    if (is.unsorted(i, strictly = TRUE)) 
+
+    if (is.unsorted(i, strictly = TRUE))
       stop("Station ids must be in obs_index order")
-    
+
     obs <- ds[vname][, i]
-    
+
   } else if ((!is.null(start_end)) & is.null(stnids)) {
-    
+
     stnids <- stns$station_id
     i <- which(times >= start_end[1] & times <= start_end[2])
     times <- times[i]
     obs <- ds[vname][i, ]
-    
+
   } else {
     i_stns <- stns@data[stnids, "obs_index"]
-    
-    if (is.unsorted(i_stns, strictly = TRUE)) 
+
+    if (is.unsorted(i_stns, strictly = TRUE))
       stop("Station ids must be in obs_index order")
-    
+
     i_time <- which(times >= start_end[1] & times <= start_end[2])
     times <- times[i_time]
     obs <- ds[vname][i_time, i_stns]
   }
-  
+
   colnames(obs) <- stnids
-  
+
   if ("missing_value" %in% h5::list.attributes(ds[vname])) {
     obs[obs == h5::h5attr(ds[vname], "missing_value")] = NA
   }
-  
+
   if ("_FillValue" %in% h5::list.attributes(ds[vname])) {
     obs[obs == h5::h5attr(ds[vname], "_FillValue")] = NA
   }
-  
+
   obs <- xts::xts(obs, times)
-  
+
   return(obs)
-  
+
 }
 
 #' Build a boolean mask for stations that have long enough period-of-record
-#' 
+#'
 #' @param ds H5File object pointing to netCDF observation file
 #' @param vname Variable name for which to load observations
 #' @param stns SpatialPointsDataFrame of station metadata from \code{\link{load_stns}}
 #' @param times POSIXct vector of times for the netCDF observation file from
 #'   \code{\link{load_time}}
 #' @param nyrs The minimum period of record in years. The function tests
-#'   whether a station has at least nyrs years of data in each month. 
-#' @param start_end Character vector of size 2 with start and end dates, 
+#'   whether a station has at least nyrs years of data in each month.
+#' @param start_end Character vector of size 2 with start and end dates,
 #'   Default: uses observations for all times in the netCDF file
 #' @param stnids Station ids for which to load observations,
 #'   Default: uses observations for all stations in the netCDF file
 #' @return boolean mask for stations
 #' @export
 build_pormask <- function(ds, vname, stns, times, nyrs, start_end = NULL, stnids = NULL) {
-  
+
   obs <- load_obs(ds, vname, stns, times, start_end, stnids)
-  
+
   # Create mask of which observations are not NA
   mask_notna <- !is.na(obs)
-  
+
   # Get mth for each observation
   mth <- xts::.indexmon(obs)+1
-  
+
   # Aggregate sum of non-NA observations by month
   obs_cnts <- aggregate.data.frame(mask_notna, by=list(mth=mth), sum)
   # Remove first column which is the months used for grouping
   obs_cnts <- obs_cnts[, -1]
-   
+
   # Get number of days in each month for a non leap year
-  mths_yr <- as.POSIXlt(seq(as.Date("2015-01-01"), 
+  mths_yr <- as.POSIXlt(seq(as.Date("2015-01-01"),
                             as.Date("2015-12-31"), by = "day"))$mon + 1
   days_in_month <- sapply(1:12, FUN = function(x) {
     sum(mths_yr == x)
   })
-  
+
   # Multiply by number years to get the min number of observations that are needed
   # for n years of data in each month
   nmin <- days_in_month * nyrs
-  
+
   # Create final por mask
   mask_por <- t(sapply(1:12, function(x) {
     obs_cnts[x, ] > nmin[x]
   }))
   mask_por <- colSums(mask_por) == 12
-  
+
   return(mask_por)
-  
+
 }
 
 #' Initialize a new netCDF observation file
-#' 
-#' @param fpath File path for the new netCDF observation file 
+#'
+#' @param fpath File path for the new netCDF observation file
 #' @param stns SpatialPointsDataFrame with station metadata
 #' @param times POSIXct vector of observation times
 #' @param vnames Vector of observation variable names (e.g.--tmin, tmax, prcp)
 #' @return A H5File object pointing to new netCDF observation file
 #' @export
 init_obsnc <- function(fpath, stns, times, vnames) {
-  
+
   # Create station_id dimension
   dim_stnid <- ncdf4::ncdim_def("station_id", "", seq(length(stns$station_id)),
                                 create_dimvar = FALSE)
-  
+
   # Create time dimension
   times <- as.POSIXct(times)
   tunits <- sprintf("days since %s", strftime(min(times), "%Y-%m-%d"))
   dim_time <- ncdf4::ncdim_def("time", tunits, RNetCDF::utinvcal.nc(tunits, times))
-  
+
   # Create station metadata variables
   vars_meta <- list()
   vals_meta <- list()
   i <- 1
-  
+
   # First loop through data variables
   for (vname in colnames(stns@data)) {
-    
+
     a_vals_meta <- stns@data[, vname]
-    
+
     if (class(a_vals_meta) == "character") {
-      
+
       # If character datatype, values must be stored in 2d variable
       char_len <- max(nchar(a_vals_meta))
-      
+
       # Create string dimension with size char_len
-      dim_string <- ncdf4::ncdim_def(paste0("string_", vname), "", seq(char_len), 
+      dim_string <- ncdf4::ncdim_def(paste0("string_", vname), "", seq(char_len),
                                      create_dimvar = FALSE)
       # Create 2-d variable
       a_var <- ncdf4::ncvar_def(vname, "", list(dim_string, dim_stnid), prec = "char")
-      
+
     } else {
-      
+
       a_var <- ncdf4::ncvar_def(vname, "", list(dim_stnid), prec = "double")
-      
+
     }
-    
+
     vars_meta[[i]] <- a_var
     vals_meta[[i]] <- a_vals_meta
     i = i + 1
-    
+
   }
-  
+
   # Add coordinate variables to the metadata
-  for (vname in colnames(coordinates(stns))) {
-    
-    a_vals <- as.numeric(coordinates(stns)[, vname])
+  for (vname in colnames(sp::coordinates(stns))) {
+
+    a_vals <- as.numeric(sp::coordinates(stns)[, vname])
     a_var <- ncdf4::ncvar_def(vname, "", list(dim_stnid), prec = "double")
-    
+
     vars_meta[[i]] <- a_var
     vals_meta[[i]] <- a_vals
     i = i + 1
-    
+
   }
-  
+
   # Create main 2D observation variables
   vars_main = list()
   i = 1
-  
+
   for (vname in vnames) {
-    a_var <- ncdf4::ncvar_def(vname, "", list(dim_stnid, dim_time), prec = "double", 
+    a_var <- ncdf4::ncvar_def(vname, "", list(dim_stnid, dim_time), prec = "double",
                               compression = 4, chunksizes = c(1, length(times)), missval = NA)
     vars_main[[i]] <- a_var
     i = i + 1
   }
-  
+
   # Create netcdf file and add station metadata
   ds <- ncdf4::nc_create(fpath, append(vars_meta, vars_main), force_v4 = T)
-  
+
   for (i in seq(length(vals_meta))) {
-    
+
     a_var <- vars_meta[[i]]
     a_vals_meta <- vals_meta[[i]]
-    
+
     ncdf4::ncvar_put(ds, a_var, a_vals_meta)
-    
+
   }
-  
+
   nc_sync(ds)
   nc_close(ds)
-  
+
   # Return h5file object pointing to the new netcdf file
   return(h5::h5file(fpath))
-  
+
 }
 
 #' Convert a tidy/longform dataframe into a spacewide xts object
-#' 
-#' @param df_tidy 
-#' @param value.var 
-#' @param time_col 
-#' @param id_col 
+#'
+#' @param df_tidy
+#' @param value.var
+#' @param time_col
+#' @param id_col
 #' @return A xts object
 #' @export
-coerce_tidy_df_to_spacewide_xts <- function(df_tidy, value.var, time_col = "time", 
+coerce_tidy_df_to_spacewide_xts <- function(df_tidy, value.var, time_col = "time",
                                             id_col = "station_id") {
-  
+
   xts_sw <- reshape2::dcast(df_tidy, as.formula(paste0(time_col, "~", id_col)), value.var = value.var)
   row.names(xts_sw) <- xts_sw[[time_col]]
   xts_sw <- xts::as.xts(xts_sw[, colnames(xts_sw) != time_col])
   return(xts_sw)
-  
+
 }
 
 #' Convert a spacewide xts object to a spacetime STFDF object
-#' 
-#' @param xts_sw 
-#' @param spdf_locs 
-#' @param varname 
+#'
+#' @param xts_sw
+#' @param spdf_locs
+#' @param varname
 #' @return A STFDF object
 #' @export
 coerce_spacewide_xts_to_STFDF <- function(xts_sw, spdf_locs, varname) {
-  
+
   # Create spacetime STFDF object.  Data needs to be a single column data.frame
   # with station id varying the fastest By default time varies the fastest with
   # as.vector call, so transpose first
-  
+
   adf <- data.frame(as.vector(t(xts_sw)))
   colnames(adf) <- varname
   a_stfdf <- spacetime::STFDF(spdf_locs, zoo::index(xts_sw), data = adf)
-  
+
   return(a_stfdf)
 }
